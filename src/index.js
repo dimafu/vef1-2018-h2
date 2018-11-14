01170fafa2402c1c1d15fdd76a01531c8d3b5e42
Þorri Már Sigurþórsson <ths34@hi.is>
2018-11-14 14:58:25 +0000

    beinagrind

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const isLecturePage = page.classList.contains('lecture-page');
 
   if (isLecturePage) {
-
+    const lecture = new Lecture();
+    lecture.load; 
   } else {
     const list = new List();
     list.load();
   }
 });
+
+/*
+function header () {
+  const header = document.createElement('header');
+  document.header.style.backgroundImage = ""
+
+  const headerimg = document.createElement(img);
+  const headergif = document.createElement(img);
+}*/
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,11 +1,33 @@
-import { empty } from './helpers';
+import { empty, el /*improtar el líka úr helpes*/ } from './helpers';
 
 export default class List {
   constructor() {
+  
     this.container = document.querySelector('.list');
+    this.url = '../lectures.json';
+  }
+ 
+  loadLectures() {
+    //sækja fyrirlestra og skila á json formi
+    // fetch(this.url);
+
+  }
+
+  renderLectures(data) {
+    //kalla á hjálparföll for creating elements
+    data.forEach((lec) => {
+      const htmlLec = el(lec);
+      htmlLectures.push(htmlLec);
+    });
+    //const htmlToRender = createHtmlFromLectures(htmlLectures);
+    return htmlLectures;
   }
 
   load() {
-    empty(this.container);
+     this.loadLectures() //hleður fyrirlestra
+     .then(getSavedLectures) // checked sign... save gögn 
+    .then(filterLectures) // gera eitthvað við fyrirlestara. gera html, filtera fyrirlestra
+    .then(renderLectures) //viljum búa til html lista af  fyrirlestrum fyrir forsíðu. kalla á fall sem festir við div
+    empty(this.container); //fjarlægja í lokin
   }
 }
